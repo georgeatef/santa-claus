2a4bca1da4714fd1c9b0ab00ab1219f1e8f49b9a
vladknp <knplink@gmail.com>
2013-11-27 12:05:46 +0200

    Підключити JS Placeholder

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-(function(){
+$(function(){
 
-// Support placeholders in IE9
-  $('input, textarea').placeholder();
+	//
+  //=====  Placeholder  =====//
+  //
+  // Support placeholders in IE9
+  //$('input, textarea').placeholder();
+
+  
 
 })();
